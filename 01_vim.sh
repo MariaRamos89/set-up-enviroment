80630e4387e28e80fc3c923c1df7fc05cabc3fa1
maria <mariaramosmontero@gmail.com>
2021-07-13 15:48:09 +0100

    :zap: Updated vim configuration -no pathogen plug-

--- 01_vim.sh
+++ 01_vim.sh
@@ -3,37 +3,15 @@
 GREEN='\e[92m'
 END='\e[0m'
 #Vim tools 
+#TODO: Do a check of the different part to update vimrc without requiring to download plug.vim every time
 if [ ! -d ~/.vim/autoload ]; then
-    echo -e "${GREEN} ################## Installing pathogen ###################### ${END}"
-    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-fi
-
-
-if [ ! -d ~/.vim/bundle/nerdtree ]; then
-    echo -e "${GREEN} ################## Installing NERDTREE ###################### ${END}"
-    git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-fi
-
-if [ ! -d ~/.vim/bundle/syntax ]; then
-    echo -e "${GREEN} ################## Installing C++ Syntax Highlighting ###################### ${END}"
-    git clone https://github.com/octol/vim-cpp-enhanced-highlight.git /tmp/vim-cpp-enhanced-highlight
-    mkdir -p ~/.vim/syntax/
-    mv /tmp/vim-cpp-enhanced-highlight/after/syntax/cpp.vim ~/.vim/syntax/cpp.vim
-    rm -rf /tmp/vim-cpp-enhanced-highlight
-fi
-
-if [ ! -d ~/.vim/bundle/vim-colorschemes ]; then
-    echo -e "${GREEN} ################## Installing Colors ###################### ${END}"
-    git clone https://github.com/flazz/vim-colorschemes.git ~/.vim/bundle/colorschemes
-fi
-
-if [ ! -d ~/.vim/bundle/vim-airline ]; then
-    echo -e "${GREEN} ################## Installing Airline ###################### ${END}"
-    git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+    echo -e "${GREEN} ################## Installing vim-plug ###################### ${END}"
+    mkdir -p ~/.vim/autoload
+    wget -O ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
 fi
 
 echo -e "${GREEN} ################## Configuring vimrc file ###################### ${END}"
 cp files/vimrc ~/.vimrc
 
+echo -e "${GREEN} ################## Open Manually VIM and put command :PlugInstall to install all the plugins in vimrc file ###################### ${END}"
 echo -e "${GREEN} ################## Finish ###################### ${END}"
--- all.sh
+++ all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y git cmake build-essential g++ gcc python-pip python-dev curl tmux vim
+sudo apt install -y git cmake build-essential g++ gcc python3-pip python-dev curl tmux vim
 
 ./01_vim.sh
 ./02_tmux.sh
