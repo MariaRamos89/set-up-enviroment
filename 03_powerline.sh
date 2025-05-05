edd25271dc88be06865fe082c5a671b654a0359e
MariaRamos <mariaramosmontero@gmail.com>
2025-05-05 23:12:14 +0100

    :boom: Fixed powerline-shell for newer python versions #1

--- 03_powerline.sh
+++ 03_powerline.sh
@@ -2,20 +2,38 @@
 
 YELLOW='\e[93m'
 END='\e[0m'
-#Vim tools 
+#Powerline tools 
 if [ ! -d ~/powerline-shell ]; then
+    echo -e "${YELLOW} ################## Installing miniforge ###################### ${END}"
+    if [ which conda == ""]; then
+        mkdir tmp
+        wget -O tmp/miniforge.sh https://github.com/conda-forge/miniforge/releases/download/25.3.0-1/Miniforge3-25.3.0-1-Linux-x86_64.sh
+        chmod +x tmp/miniforge.sh
+        ./tmp/miniforge.sh
+    fi
+
     echo -e "${YELLOW} ################## Installing Powerline ###################### ${END}"
     git clone https://github.com/banga/powerline-shell ~/powerline-shell
-
-    echo -e "${YELLOW} ################## Configuring .bashrc file ###################### ${END}"
-    cat ${HOME}/set-up-enviroment/files/powerline-bash.txt  >> ~/.bashrc
-    source ~/.bashrc
 
     echo -e "${YELLOW} ################## Fonts ###################### ${END}"
     sudo apt install -y fonts-powerline
 
+    echo "Copy patch to folder"
+    mkdir ~/powerline-shell/patch
+    cp powerline-patch/* ~/powerline-shell/patch/.
+
+
+    echo -e "${YELLOW} ################## Configuring .bashrc file ###################### ${END}"
+    cat files/powerline-bash.txt  >> ~/.bashrc
+    source ~/.bashrc
+
+    echo -e "${YELLOW} ################## Applying patch ###################### ${END}"
     cd ~/powerline-shell
-    echo -e "DO  'sudo python setup.py install' to finish the installation on powerline"
+    echo -e "Applyint patch for newer versions"
+    patch -p1 -N < patch/fix.patch
+    # sudo python setup.py install # Deprecated
+    pip install .
+
 fi
 
 echo -e "${YELLOW} ################## Finish ###################### ${END}"
--- all.sh
+++ all.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-sudo apt install -y git cmake build-essential g++ gcc python3-pip python-dev curl tmux vim
+echo "Selected system $1"
+
+if [ $# -eq 0 ]
+then
+    echo "No arguments passed. Please write your linux distro: debian or arch"
+    exit 1
+fi
+
+if [ "$1" == "debian" ] || [ "$1" == "ubuntu" ]
+then
+    sudo apt install -y git cmake build-essential gcc python-pip python curl tmux vim wget
+elif [ "$1" == "arch" ]
+then
+    sudo pacman -S git cmake gcc python-pip python curl tmux vim wget
+else 
+    echo "Only debian distros or arch linux are available"
+    echo "Choose: debian or arch"
+    exit 1
+fi
+
 
 ./01_vim.sh
 ./02_tmux.sh
